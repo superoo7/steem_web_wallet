de1f4f07323d4d276a64a06e411a55397ea25c2d
Lai Weng Han <jlwhoo7@gmail.com>
2018-09-21 20:33:54 +0800

    change Index to index

--- src/Page/Home/index.tsx
+++ src/Page/Home/index.tsx
@@ -1,6 +1,6 @@
-import SignIn from 'Component/Shared/SignIn';
 import * as React from 'react';
 import { RouteComponentProps, Link } from '@reach/router';
+import SignIn from 'Component/Shared/SignIn';
 
 class HomePage extends React.Component<RouteComponentProps> {
     render() {
---
+++ webpack.prod.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    resolve: {
+        modules: [path.resolve(__dirname, "src"), 'node_modules'],
+        extensions: [".ts", ".tsx", ".js", ".json"],
+        alias: {
+            Utils: path.resolve(__dirname, 'src/Utils/'),
+            Steem: path.resolve(__dirname, 'src/Utils/Steem'),
+            Component: path.resolve(__dirname, 'src/Component'),
+        },
+    },
+    entry: path.resolve(__dirname, 'src/index.tsx'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: ''
+    },
+    module: {
+        rules: [{
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader'
+            },
+            {
+                test: /\.tsx?$/,
+                use: [
+                    "babel-loader",
+                    'awesome-typescript-loader'
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.EnvironmentPlugin({
+            'NODE_ENV': 'development'
+        }),
+        new CopyWebpackPlugin([{
+                from: path.resolve(__dirname, 'src/index.html')
+            },
+            {
+                from: path.resolve(__dirname, 'public/_redirects')
+            },
+        ]),
+        // { from: path.resolve(__dirname, 'browserconfig.xml'), to: 'assets' },
+        // { from: path.resolve(__dirname, 'src/icons'), to: 'assets/icons' }
+    ]
+}
