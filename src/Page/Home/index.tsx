e48982ace941ad8aa2b5d1c09cdb9c5440ee53e8
Lai Weng Han <jlwhoo7@gmail.com>
2018-09-21 20:41:27 +0800

    Try adding index for build

--- src/Page/Home/index.tsx
+++ src/Page/Home/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { RouteComponentProps, Link } from '@reach/router';
-import SignIn from 'Component/Shared/SignIn';
+import SignIn from 'Component/Shared/SignIn/index';
 
 class HomePage extends React.Component<RouteComponentProps> {
     render() {
