ac4d478e0be2261755c8020d47b6fb825fcabafc
Lai Weng Han <jlwhoo7@gmail.com>
2018-09-22 17:33:55 +0800

    fix styling on wallet and faq

--- src/Page/Profile/index.tsx
+++ src/Page/Profile/index.tsx
@@ -5,10 +5,25 @@ import Wallet from 'Component/Shared/SignIn/Wallet';
 interface IProfilePageProps extends RouteComponentProps {}
 
 class ProfilePage extends React.Component<IProfilePageProps> {
+    constructor(props: IProfilePageProps) {
+        super(props);
+    }
+
     render() {
         const props: any = this.props;
-        console.log(props);
-        const username = props.username;
+        const username: string = props.username;
+        // ?currency=STEEM&amount=20&to=superoo7
+        const search: string = props.location.search;
+
+        if (search) {
+            const _currency: RegExpMatchArray | [string, string] = search.match(/currency=([^&]*)/) || ['', ''];
+            const _amount: RegExpMatchArray | [string, string] = search.match(/amount=([^&]*)/) || ['', ''];
+            const _to: RegExpMatchArray | [string, string] = search.match(/name=([^&]*)/) || ['', ''];
+            const currency = _currency[1];
+            const amount = _amount[1];
+            const to = _to[1];
+        }
+
         return <Wallet username={username} />;
     }
 }
